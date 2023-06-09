86abaf22c8996a0053691f92859a9411d987df12
mfried95 <m.friedman1995@gmail.com>
2023-06-09 17:53:24 -0400

    fixed error server now running

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Router = require("./routes");
 
 const app = express();
 
-const port = 3000;
-
 app.use(express.json());
 
+mongoose.connect('mongodb+srv://cozeycradle:s2EpenbL4JpBBABN@cozeycradle.w07iyp9.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect('mongodb+srv://cozeycradle:s2EpenbL4JpBBABN@cozeycradle.w07iyp9.mongodb.net/',
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }
-);
-
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
