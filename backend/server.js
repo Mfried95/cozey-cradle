76d7d8322cf427f4686d30fddd3e9de086c408fd
mfried95 <m.friedman1995@gmail.com>
2023-06-10 12:58:32 -0400

    pulled data from mongodb into /movies route

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,36 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
 const app = express();
+const port = 3000;
 
-app.use(express.json());
+const uri = "mongodb+srv://cozeycradle:s2EpenbL4JpBBABN@cozeycradle.w07iyp9.mongodb.net/";
+const client = new MongoClient(uri);
 
-mongoose.connect('mongodb+srv://cozeycradle:s2EpenbL4JpBBABN@cozeycradle.w07iyp9.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const connectDb = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+};
+
+connectDb();
+
+app.get("/movies", async(req, res) => {
+  try {
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+    const allMovies = await movies.find().toArray();
+    res.json(allMovies);
+  } catch (error) {
+    console.error("Failed to fetch movies from the database:", error);
+    res.status(500).json({ error: "Failed to fetch movies from the database" });
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
 
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
