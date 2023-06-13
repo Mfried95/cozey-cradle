59eda451bd8e305338f9aabe23ae07d5bf826db6
mfried95 <m.friedman1995@gmail.com>
2023-06-13 15:57:49 -0400

    Updated navbar links effects

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <h2>Cozey Cradle</h2>
-      <div className="nav-links">
+      <div className="nav-links " >
         <Link to="/">Home</Link>
         <Link to="/Cradles">Cradles</Link>
         <Link to="/Works">How it works</Link>
