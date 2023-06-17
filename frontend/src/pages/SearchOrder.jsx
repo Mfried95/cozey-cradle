06bbbe4af9c12d584baf04d837ea5c36d869eef3
mfried95 <m.friedman1995@gmail.com>
2023-06-17 14:31:16 -0400

    Search by Order ID and then fetch the product id and both are displaying on the /serach page - need to style and add specific data next

--- frontend/src/pages/SearchOrder.jsx
+++ frontend/src/pages/SearchOrder.jsx
@@ -7,6 +7,7 @@ const SearchOrder = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [matchedBooking, setMatchedBooking] = useState(null);
   const [error, setError] = useState("");
+  const [productData, setProductData] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3000/bookings")
@@ -14,12 +15,12 @@ const SearchOrder = () => {
       .then((data) => {
         setBookings(data);
       })
-      .catch((error) =>
-        console.error("Failed to fetch bookings:", error)
-      );
+      .catch((error) => console.error("Failed to fetch bookings:", error));
   }, []);
 
-  const handleSearch = () => {
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
     if (searchTerm.length !== 5) {
       setError("Must enter a 5-digit booking ID");
       return;
@@ -32,6 +33,16 @@ const SearchOrder = () => {
     if (foundBooking) {
       setMatchedBooking(foundBooking);
       setError("");
+
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/products/${foundBooking.productID}`
+        );
+        const product = await response.json();
+        setProductData(product);
+      } catch (error) {
+        console.error("Failed to fetch product data:", error);
+      }
     } else {
       setMatchedBooking(null);
       setError("No match found");
@@ -42,7 +53,7 @@ const SearchOrder = () => {
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
-    setError('')
+    setError("");
   };
 
   const clearSearchTerm = () => {
@@ -50,26 +61,49 @@ const SearchOrder = () => {
   };
 
   return (
-    <div>
-      <TextField
-        label="Search by booking ID"
-        variant="outlined"
-        value={searchTerm}
-        onChange={handleInputChange}
-        onClick={clearSearchTerm}
-      />
-      <Button variant="contained" onClick={handleSearch}>
-        Search
-      </Button>
-      {matchedBooking ? (
-        <ul>
-          <li key={matchedBooking._id}>{matchedBooking._id}</li>
-          {console.log(matchedBooking)}
-        </ul>
-      ) : (
-        <p>{error}</p>
-      )}
-    </div>
+    <form onSubmit={handleSearch}>
+    <TextField
+      label="Search by booking ID"
+      variant="outlined"
+      value={searchTerm}
+      onChange={handleInputChange}
+      onClick={clearSearchTerm}
+    />
+    <Button variant="contained" type="submit">
+      Search
+    </Button>
+    {matchedBooking ? (
+      <div className="bookings-container">
+        <h2>Order details for ID: {matchedBooking._id.slice(-4)}</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Product Image</th>
+              <th>Product Name</th>
+              <th>Product Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                {productData && (
+                  <img
+                    src={productData.image}
+                    alt={productData.name}
+                    style={{ width: "100px" }}
+                  />
+                )}
+              </td>
+              <td>{productData && productData.name}</td>
+              <td>{productData && productData.duration}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    ) : (
+      <p>{error}</p>
+    )}
+  </form>
   );
 };
 
