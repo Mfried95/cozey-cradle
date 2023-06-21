ccc94dd730e47cdc47c572d6864131c91bba7dab
mfried95 <m.friedman1995@gmail.com>
2023-06-21 17:49:54 -0400

    changes vite config to add env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config'
+
+
+
+
+
+console.log(`port env is ${process.env.PORT}`)
+
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(
+  Object.assign(
+    {
+      plugins: [react()],
+    },
+    process.env.PORT
+      ? {
+          server: {
+            port: process.env.PORT,
+          },
+        }
+      : {}
+  )
+);
